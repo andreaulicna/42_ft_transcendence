a35dde541e4fee0ce6dc1715cb8c28d3e3e58da9
Petr Louda <peta.louda95@gmail.com>
2024-11-06 12:17:43 +0100

    fixed pong container not starting properly

--- pong/pong/settings.py
+++ pong/pong/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-PONG_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
