acdee4dd8a57fd87edb56d6d26fc67c2f4dfb3ec
Andrea Uličná <andrea@ulicna.com>
2024-11-23 23:11:22 +0100

    Contributes to #28 - join tournament view. api/tournament/join endpoint if tournament_name: assumes tournament with friends, so adds according to the name if not tournament_name: assumes random tournament, so adds to the first available only one tournament waiting or in-progress

--- tournament/api/views.py
+++ tournament/api/views.py
@@ -45,8 +45,76 @@ class CreateTournamentView(APIView):
 						'player_tournament' : player_tournament_serializer.data},
 						status=status.HTTP_201_CREATED)
 
+def player_already_in_a_tournament(tournaments, player_id):
+    for tournament_iterator in tournaments:
+        players_in_tournament = PlayerTournament.objects.filter(tournament=tournament_iterator.id)
+        for player in players_in_tournament:
+            if player.player_id == player_id:
+                return True
+    return False
+class JoinTournamentView(APIView):
+	permission_classes = [IsAuthenticated]
+	def post(self, request):
+		# Get data from the request
+		player_tmp_username = request.data.get('player_tmp_username')
+		tournament_name = request.data.get('tournament_name')
+
+		# Allow only one waiting or in-progress tournament
+		waiting_and_inprogress_tournament = Tournament.objects.filter(
+			Q(status=Tournament.StatusOptions.WAITING) | Q(status=Tournament.StatusOptions.INPROGRESS)
+		)
+		if player_already_in_a_tournament(waiting_and_inprogress_tournament, request.user.id) == True:
+			return Response({"details" : "You are alredy in a (waiting or in progress) tournament!"}, status=status.HTTP_403_FORBIDDEN)
+
+		# Get all waiting tournaments
+		waiting_tournaments = Tournament.objects.filter(status=Tournament.StatusOptions.WAITING)
+		# Try to find a tournament to join 
+		for waiting_tournament in waiting_tournaments: 
+			players_in_tournament = PlayerTournament.objects.filter(tournament=waiting_tournament.id)
+			# If there was a tournament_name supplied, assume tournament between friends
+			if (tournament_name is not None):
+				if (waiting_tournament.name != tournament_name):
+					continue
+				if (len(players_in_tournament) == 4): # CHANGE to tournament capacity
+					return Response({'detail': 'The requested tournament is already full!'}, status=status.HTTP_200_OK)
+				player_tournament_data = {'player' : request.user.id,
+										'tournament' : waiting_tournament.id}
+				if player_tmp_username:
+					player_tournament_data['player_tmp_username'] = player_tmp_username
+				player_tournament_serializer = PlayerTournamentSerializer(data=player_tournament_data)
+				if player_tournament_serializer.is_valid():
+					player_tournament_serializer.save()
+				else:
+					return Response(player_tournament_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+				return Response({'detail': 'You can join your friends at this tournament!.', 
+						   		'tournament': TournamentSerializer(waiting_tournament).data,
+								'player_tournament' : player_tournament_serializer.data},
+								status=status.HTTP_200_OK)
+			# If there wasn't a tournament_name supplied, asuume random tournament
+			elif (len(players_in_tournament) < 4): # CHANGE to tournament capacity
+				player_tournament_data = {'player' : request.user.id,
+										'tournament' : waiting_tournament.id}
+				if player_tmp_username:
+					player_tournament_data['player_tmp_username'] = player_tmp_username
+				player_tournament_serializer = PlayerTournamentSerializer(data=player_tournament_data)
+				if player_tournament_serializer.is_valid():
+					player_tournament_serializer.save()
+				else:
+					return Response(player_tournament_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+				return Response({'detail': 'Found a tournamet to join for you!.', 
+						   		'tournament': TournamentSerializer(waiting_tournament).data,
+								'player_tournament' : player_tournament_serializer.data},
+								status=status.HTTP_200_OK)
+		if tournament_name:
+			return Response({'detail': 'There is no tournament with such a name. You need to create it to play with your friends!'}, status=status.HTTP_200_OK)
+		return Response({'detail': 'You need to create a tournament to join!'}, status=status.HTTP_200_OK)
 		
 class TournamentListView(ListAPIView):
 	serializer_class = TournamentSerializer
 	permission_classes = [IsAuthenticated]
 	queryset = Tournament.objects.all()
+
+class PlayerTournamentListView(ListAPIView):
+	serializer_class = PlayerTournamentSerializer
+	permission_classes = [IsAuthenticated]
+	queryset = PlayerTournament.objects.all()
