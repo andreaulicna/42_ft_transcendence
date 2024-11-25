82e6d2ca3c4c34bbf08b96b2952ec5e9080076e3
Petr Louda <peta.louda95@gmail.com>
2024-11-25 18:27:43 +0100

    New pong logic with vectors
    also changed ball diameter to real radius in JS
    
    Co-authored-by: Andrea Uličná <andreaulicna@users.noreply.github.com>

--- pong/api/consumers.py
+++ pong/api/consumers.py
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    consumers.py                                       :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: plouda <plouda@student.42prague.com>       +#+  +:+       +#+         #
+#    By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/28 14:23:42 by plouda            #+#    #+#              #
-#    Updated: 2024/11/29 16:11:19 by plouda           ###   ########.fr        #
+#    Updated: 2024/12/04 13:44:14 by vbartos          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
