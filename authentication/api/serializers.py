d2e58c6e18b9959e083544a73f84133fed23412a
Petr Louda <peta.louda95@gmail.com>
2025-01-09 17:09:55 +0100

    Remote authentication WIP, see comments above IntraCallbackView

--- authentication/api/serializers.py
+++ authentication/api/serializers.py
@@ -1,6 +1,10 @@
 from rest_framework_simplejwt.serializers import TokenRefreshSerializer 
 from rest_framework_simplejwt.exceptions import InvalidToken
 from django.conf import settings
+from .models import CustomUser
+from rest_framework import serializers
+
+
 
 class CustomTokenRefreshSerializer(TokenRefreshSerializer):
 	refresh = None
@@ -12,3 +16,14 @@ class CustomTokenRefreshSerializer(TokenRefreshSerializer):
 			return super().validate(attrs)
 		else:
 			raise InvalidToken(f'No valid token found in cookie {refresh_token}')
+		
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = CustomUser
+		fields = '__all__' # returns all parameters
+		extra_kwargs = {'password' : {'write_only': True}, 
+						'two_factor_secret' : {'write_only' : True}} # protects GET user/info/ from exposing the hash of the password in the response
+
+	def create(self, validated_data):
+		user = CustomUser.objects.create_user(**validated_data)
+		return user
