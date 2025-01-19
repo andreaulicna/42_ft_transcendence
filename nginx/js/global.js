a9d20bcbf0fcc1faee5ce540f6e56bfa16333ec2
sl16 <vojtabar119@gmail.com>
2025-01-19 12:27:57 +0100

    frontend - add delay to grace period reconnect attempt so the refresh token is fetched first

--- nginx/js/global.js
+++ nginx/js/global.js
@@ -1,10 +1,13 @@
+import { delay } from "./gameCore.js";
 import { openPongWebsocket } from "./websockets.js";
 
-function reconnectToGame()
+async function reconnectToGame()
 {
 	if (localStorage.getItem("in_game") == "YES" && localStorage.getItem("match_id"))
 	{
 		console.log("Status 'in_game' is set to YES, attempting to reconnect if still valid.");
+		// Wait for the refresh token before attempting to open a websocket
+		await delay(100);
 		openPongWebsocket(localStorage.getItem("match_id"));
 	}
 }
