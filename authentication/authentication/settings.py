82441def145381fc0f14a337f621ef4892db9cfd
Petr Louda <peta.louda95@gmail.com>
2025-01-21 13:33:37 +0100

    Set refresh_token lifetime to 1 hour.
    Also removes all references to AUTH_COOKIE in other settings.py.

--- authentication/authentication/settings.py
+++ authentication/authentication/settings.py
@@ -94,7 +94,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(seconds=10),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(hours=1),
     'ROTATE_REFRESH_TOKENS': True,
 	'BLACKLIST_AFTER_ROTATION' : True,
 	'AUTH_COOKIE': 'refresh_token',  # Cookie name. Enables cookies if value is set.
