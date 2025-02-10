cafca6fd011e5efdedf7d641e4e0e3126d52619d
Petr Louda <peta.louda95@gmail.com>
2025-02-10 11:32:51 +0100

    Add missing locale middlewares

--- pong/pong/settings.py
+++ pong/pong/settings.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
 	"debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.locale.LocaleMiddleware',
 	'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
